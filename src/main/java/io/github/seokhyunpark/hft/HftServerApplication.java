ec604e636999c747c166d529642228805f118e13
skhnpark <skhnpark125@gmail.com>
2025-11-28 18:41:11 +0900

    init: 프로젝트 초기 설정

---
+++ src/main/java/io/github/seokhyunpark/hft/HftServerApplication.java
@@ -0,0 +1,13 @@
+package io.github.seokhyunpark.hft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HftServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HftServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/seokhyunpark/hft/HftServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.seokhyunpark.hft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HftServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
