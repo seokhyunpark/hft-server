50cacee32a3d36c5f1ebd3f44b74045584dc54fd
skhnpark <skhnpark125@gmail.com>
2026-01-12 03:07:34 +0900

    feat: TradingProperties 설정 활성화

--- src/main/java/io/github/seokhyunpark/hft/HftServerApplication.java
+++ src/main/java/io/github/seokhyunpark/hft/HftServerApplication.java
@@ -2,8 +2,12 @@ package io.github.seokhyunpark.hft;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import io.github.seokhyunpark.hft.trading.config.TradingProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(TradingProperties.class)
 public class HftServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(HftServerApplication.class, args);
