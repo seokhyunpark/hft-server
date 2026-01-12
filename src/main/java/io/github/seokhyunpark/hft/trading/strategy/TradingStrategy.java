e66962b1c70207c1a003d1cfc8e886ef906b7f36
skhnpark <skhnpark125@gmail.com>
2026-01-13 01:28:03 +0900

    feat: 트레이딩 전략을 관리하는 TradingStrategy 생성 및 매수 주문 전략 구현

---
+++ src/main/java/io/github/seokhyunpark/hft/trading/strategy/TradingStrategy.java
@@ -0,0 +1,61 @@
+package io.github.seokhyunpark.hft.trading.strategy;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Comparator;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import io.github.seokhyunpark.hft.exchange.dto.stream.PartialBookDepth;
+import io.github.seokhyunpark.hft.trading.config.TradingProperties;
+import io.github.seokhyunpark.hft.trading.dto.OrderParams;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class TradingStrategy {
+    private final TradingProperties tradingProperties;
+
+    // ----------------------------------------------------------------------------------------------------
+    // 매수 주문 전략
+    // ----------------------------------------------------------------------------------------------------
+    public OrderParams calculateBuyOrderParams(PartialBookDepth depth) {
+        BigDecimal price = calculateBuyPrice(depth);
+        if (price.compareTo(BigDecimal.ZERO) <= 0) {
+            return new OrderParams(BigDecimal.ZERO, BigDecimal.ZERO);
+        }
+        BigDecimal qty = calculateBuyQty(price);
+        return new OrderParams(price, qty);
+    }
+
+    private BigDecimal calculateBuyPrice(PartialBookDepth depth) {
+        return depth.bids().stream()
+                .max(Comparator.comparing(bid -> new BigDecimal(bid.getLast())))
+                .filter(this::isStrongBuyWall)
+                .map(this::applyPriceOffset)
+                .orElse(BigDecimal.ZERO);
+    }
+
+    private boolean isStrongBuyWall(List<String> bid) {
+        BigDecimal price = new BigDecimal(bid.getFirst());
+        BigDecimal qty = new BigDecimal(bid.getLast());
+        BigDecimal usd = price.multiply(qty);
+
+        return usd.compareTo(tradingProperties.buyWallThresholdUsd()) >= 0;
+    }
+
+    private BigDecimal applyPriceOffset(List<String> bid) {
+        BigDecimal rawPrice = new BigDecimal(bid.getFirst()).add(tradingProperties.priceTickSize());
+        return rawPrice.setScale(tradingProperties.priceTickSize().scale(), RoundingMode.FLOOR);
+    }
+
+    private BigDecimal calculateBuyQty(BigDecimal price) {
+        if (price.compareTo(BigDecimal.ZERO) <= 0) {
+            return BigDecimal.ZERO;
+        }
+        return tradingProperties.minOrderSize().divide(price, tradingProperties.qtyTickSize().scale(), RoundingMode.CEILING);
+    }
+}
