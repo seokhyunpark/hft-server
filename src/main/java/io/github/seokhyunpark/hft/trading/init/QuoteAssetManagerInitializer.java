2c04a4dde4d29fbe925d2ce2fa2fbca4933a3d3c
skhnpark <skhnpark125@gmail.com>
2026-01-21 01:17:51 +0900

    refactor: 클래스 명 변경 (AssetManagerInitializer -> QuoteAssetManagerInitializer) 및 오류 발생 시 시스템을 안전하게 종료하도록 변경

--- src/main/java/io/github/seokhyunpark/hft/trading/init/QuoteAssetManagerInitializer.java
+++ src/main/java/io/github/seokhyunpark/hft/trading/init/QuoteAssetManagerInitializer.java
@@ -2,8 +2,9 @@ package io.github.seokhyunpark.hft.trading.init;
 
 import java.math.BigDecimal;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
@@ -12,28 +13,31 @@ import lombok.extern.slf4j.Slf4j;
 
 import io.github.seokhyunpark.hft.exchange.client.BinanceClient;
 import io.github.seokhyunpark.hft.exchange.dto.rest.GetAccountResponse.Balance;
+import io.github.seokhyunpark.hft.trading.config.TradingProperties;
 import io.github.seokhyunpark.hft.trading.manager.QuoteAssetManager;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
-public class AssetManagerInitializer {
+public class QuoteAssetManagerInitializer {
     private final BinanceClient binanceClient;
     private final QuoteAssetManager quoteAssetManager;
-
-    @Value("${hft.trading.quote-asset}")
-    private String quoteAsset;
+    private final TradingProperties tradingProperties;
+    private final ConfigurableApplicationContext context;
 
     @EventListener(ApplicationReadyEvent.class)
     public void initialize() {
-        Balance quoteBalance = binanceClient.getBalance(quoteAsset);
+        Balance quoteBalance = binanceClient.getBalance(tradingProperties.quoteAsset());
         if (quoteBalance == null || quoteBalance.free() == null) {
-            log.debug("[INIT-ASSET-FAIL] AssetManager 초기화 실패");
+            int exitCode = SpringApplication.exit(context, () -> 1);
+            System.exit(exitCode);
+
+            log.error("[INIT-QUOTE-ASSET-FAIL] AssetManager 초기화 실패");
             return;
         }
 
         BigDecimal balance = new BigDecimal(quoteBalance.free());
         quoteAssetManager.syncQuoteBalance(balance);
-        log.debug("[INIT-ASSET-SUCCESS] AssetManager 초기화 성공");
+        log.debug("[INIT-QUOTE-ASSET-SUCCESS] AssetManager 초기화 성공");
     }
 }
